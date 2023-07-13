db77e1f60577cdad771bc76b527345d0d0ff5f34
TimmyChannel <timmykanal@gmail.com>
2023-07-13 20:53:29 +0300

    Add project files.

---
+++ Models/CalendarEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Schedule.Models
+{
+    public class CalendarEvent
+    {
+        public int Id { get; set; }
+        public int TeacherID { get; set; }
+        public int SubjectID { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+    }
+}
